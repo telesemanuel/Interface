e93b6c0d168ba9b1b5f372e9eda638baa842fabf
telesemanuel <emanuelteles06@gmail.com>
2024-09-17 17:24:24 -0300

    v

--- app.py
+++ app.py
@@ -3,46 +3,56 @@ from modulo import *
 if __name__ == "__main__":
   
     #construtor - instaciando o objeto da subclasse
-    cc = ContaCorrente('', '', '1001-1', '10010-1', 0)
+    cc = ContaCorrente(" ", " ", "1001-1", "10010-1", 0)
 
     #recebendo dados
     cc.nome = input('Informe o nome do titular: ')
     cc.cpf = input('Informe o cpf do titular: ')
 
-    print('\n')
-    print(f'{'-'*10}Dados do Titular{'-'*10}')
-    print(f'Titular da conta: {cc.nome}.')
-    print(f'CPF: {cc.cpf}.')
-    print(f'Agencia: {cc.agencia}.')
-    print(f'Conta: {cc.conta}.')
-    
     while True:
         print('\n')
-        print(f'\n{'=' * 20} Serpent Bank |{'=' * 20}\n')
-
+        print(f"{'-'*10}Dados do Titular{'-'*10}")
+        print(f"Titular da conta: {cc.nome}.")
+        print(f"CPF: {cc.cpf}.")
+        print(f"Conta: {cc.conta}.")
+        print(f"Agencia: {cc.agencia}.")
+        print(f"Saldo: R$ {cc.saldo}")
         
-        print('1 - Saldo')
-        print('2 - Depósito')
-        print('3 - Saque')
-        print('0 - Sair')
+        print(f"\n{cc.nome} deseja realizar qual operação?")
+        print("1 - saldo")
+        print("2 - depósito")
+        print("3 - Saque")
+        print("4 - Sair")
+        
+        op = input("escolha uma opcao: ")
+        match op:
+            case "1":
+                print("Consultar Saldo\n")
+                print(f"Saldo disponível R$ {cc.consultar_saldo():,.2f}.")
+            case "2":
+                valor = float(input(" Valor depósito: R$").replace(",","."))
+                if valor > 0:
+                    cc.saldo = cc.fazer_deposito(valor)
+                    print("Depósito efetuado com sucesso.")
+                    print(f"Saldo atual: R$ {cc.consultar_saldo():,.2f}")
+                else:
+                    print("Valor inválido")
+                    continue
+            case "3":
+                valor = float(input("Valor do Saque: R$ ").replace(",","."))
+                if valor <= cc.saldo:
+                    cc.saldo = cc.fazer_saque(valor)
+                    print("Saque efetuado com sucesso.")
+                    print(f"Saldo atual: R$ {cc.consultar_saldo:,.2f}")
+                else:
+                    print("Não foi possível efetuar saque.")
+            case "4":
+                print("Saindo...")
+                break
+            case _:
+                print("Opção Inválida")
 
-        op = input('Informe a ação que deseja fazer: ')
 
-        if op == '1':
-            print('Saldo em conta R$ {cc.saldo}')
-            continue
-        elif op == '2':
-            deposito = int(input('Informe o valor do depósito: '))
-            cc.fazer_deposito(deposito)
-            print(f'Depósito efetuado com sucesso!')
-            continue
-        elif op == '3':
-            saque = int(input('Informe o valor do saque: '))
-            cc.fazer_saque(saque)
-            print(f'Saque efetuado no valor de R$ {saque:,.2f}.')
-            continue
-        else:
-            break
 
 
 
